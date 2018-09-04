cba42334464611fb3b12c2747789588ae8db929e
CrimsKrams <krille21@live.com>
2018-09-04 13:23:20 +0200

    bakgfrund

---
+++ map/bush.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="bush" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="bush.png" trans="ff00ff" width="70" height="70"/>
+</tileset>
---
+++ map/sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sheet" tilewidth="32" tileheight="32" tilecount="450" columns="30">
+ <image source="sheet.png" trans="ff00ff" width="980" height="490"/>
+</tileset>
