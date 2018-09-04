6a52012868864c0ce158c2fed401e9a0ae7cbf73
Max Gotenstam <max.gotenstam@elev.ga.ntig.se>
2018-09-04 10:51:56 +0200

    renamed game file

--- BeeSouls/BeeSouls/BeeSoulsGame.cs
+++ BeeSouls/BeeSouls/BeeSoulsGame.cs
@@ -7,12 +7,12 @@ namespace BeeSouls
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class BeeSoulsGame : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public BeeSoulsGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- BeeSouls/BeeSouls/Program.cs
+++ BeeSouls/BeeSouls/Program.cs
@@ -14,7 +14,7 @@ namespace BeeSouls
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new BeeSoulsGame())
                 game.Run();
         }
     }
