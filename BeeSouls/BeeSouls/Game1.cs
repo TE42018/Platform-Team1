0a658a9ac0be5399c579af8ab93fb5440e8a21d4
CrimsKrams <krille21@live.com>
2018-09-05 11:27:09 +0200

    game menu

--- BeeSouls/BeeSouls/Game1.cs
+++ BeeSouls/BeeSouls/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -13,15 +14,34 @@ namespace BeeSouls
         SpriteBatch spriteBatch;
         TileEngine tileEngine;
 
+
+        MouseState mouseState, previousMouseState;
+        KeyboardState ks;
+        Color col;
+
+        const byte MENU = 0, PLAYGAME = 1, GAMEOVER = 2, HIGHSCORE = 3, OPTIONS = 4;
+        int CurrentScreen = MENU;
+
+        //Variables for the MENU Screen
+        Texture2D highscoreText, optionText, playgameText;
+        Button playGameButton, optionsButton, highscoreButton;
+        float screenwidth, screenheight;
+        Texture2D bgimage;
+
         public Game1()
         {
             tileEngine = new TileEngine(this);
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            this.IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
+
+            col = Color.White;
+            screenheight = graphics.GraphicsDevice.Viewport.Height;
+            screenwidth = graphics.GraphicsDevice.Viewport.Width;
             tileEngine.TileHeight = 70;
             tileEngine.TileWidth = 70;
             tileEngine.Data = new int[,]
@@ -46,6 +66,11 @@ namespace BeeSouls
             base.Initialize();
         }
 
+        internal void ChangeState(MenuState menuState)
+        {
+            throw new NotImplementedException();
+        }
+
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
@@ -55,6 +80,21 @@ namespace BeeSouls
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             tileEngine.TileMap = Content.Load<Texture2D>("tilemap");
+            //Things we want to load in the MENU screen.
+            highscoreText = Content.Load<Texture2D>("highscores");
+            optionText = Content.Load<Texture2D>("options");
+            playgameText = Content.Load<Texture2D>("PlayGame");
+            bgimage = Content.Load<Texture2D>("main menu");
+
+            highscoreButton = new Button(new Rectangle(300, 300, highscoreText.Width, highscoreText.Height), true);
+            highscoreButton.load(Content, "highscores");
+
+            optionsButton = new Button(new Rectangle(300, 200, optionText.Width, optionText.Height), true);
+            optionsButton.load(Content, "options");
+
+            playGameButton = new Button(new Rectangle(300, 100, playgameText.Width, playgameText.Height), true);
+            playGameButton.load(Content, "PlayGame");
+
         }
 
         /// <summary>
@@ -73,8 +113,11 @@ namespace BeeSouls
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.P))
                 Exit();
+            //Checking the state of our mouse.
+            mouseState = Mouse.GetState();
+            ks = Keyboard.GetState();
 
             KeyboardState state = Keyboard.GetState();
             if (state.IsKeyDown(Keys.Down))
@@ -90,6 +133,63 @@ namespace BeeSouls
             if (state.IsKeyDown(Keys.PageUp))
                 tileEngine.Zoom -= 0.05f;
 
+            switch (CurrentScreen)
+            {
+                case MENU:
+                    //What we want to happen in the MENU screen goes in here.
+                    //GO TO PLAYGAME SCREEN
+                    if (playGameButton.update(new Vector2(mouseState.X, mouseState.Y)) == true && mouseState != previousMouseState && mouseState.LeftButton == ButtonState.Pressed)
+                    {
+                        CurrentScreen = PLAYGAME;
+                    }
+
+                    //GO TO OPTIONS SCREEN
+                    if (optionsButton.update(new Vector2(mouseState.X, mouseState.Y)) == true && mouseState != previousMouseState && mouseState.LeftButton == ButtonState.Pressed)
+                    {
+                        CurrentScreen = OPTIONS;
+                    }
+
+                    //GO TO HIGHSCORE SCREEN
+                    if (highscoreButton.update(new Vector2(mouseState.X, mouseState.Y)) == true && mouseState != previousMouseState && mouseState.LeftButton == ButtonState.Pressed)
+                    {
+                        CurrentScreen = HIGHSCORE;
+                    }
+
+                    break;
+
+                case OPTIONS:
+                    //Whatever Options you want to DISPLAY
+                    if (ks.IsKeyDown(Keys.Escape))
+                    {
+                        CurrentScreen = MENU;
+                    }
+                    break;
+
+                case HIGHSCORE:
+                    if (ks.IsKeyDown(Keys.Escape))
+                    {
+                        CurrentScreen = MENU;
+                    }
+                    break;
+
+
+
+                case PLAYGAME:
+                    //What we want to happen when we play our GAME goes in here.
+                    if (ks.IsKeyDown(Keys.Escape))
+                    {
+                        CurrentScreen = MENU;
+                    }
+                    break;
+
+                case GAMEOVER:
+                    //What we want to happen when our GAME is OVER goes in here.
+                    break;
+
+
+
+            }
+            previousMouseState = mouseState;
             base.Update(gameTime);
         }
 
@@ -103,6 +203,38 @@ namespace BeeSouls
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
+            switch (CurrentScreen)
+            {
+                case MENU:
+                    //What we want to happen in the MENU screen goes in here.
+                    spriteBatch.Draw(bgimage, new Rectangle(0, 0, bgimage.Width, bgimage.Height), Color.White);
+                    spriteBatch.Draw(playgameText, new Rectangle(300, 100, playgameText.Width, playgameText.Height), Color.White);
+                    spriteBatch.Draw(optionText, new Rectangle(300, 200, optionText.Width, optionText.Height), Color.White);
+                    spriteBatch.Draw(highscoreText, new Rectangle(300, 300, highscoreText.Width, highscoreText.Height), Color.White);
+                    // spriteBatch.Draw(bgimage, new Rectangle(800, 420, bgimage.Width, bgimage.Height), Color.White);
+                    break;
+
+                case OPTIONS:
+                    //Whatever Options you want to DISPLAY
+                    spriteBatch.Draw(optionText, new Rectangle(300, 200, optionText.Width, optionText.Height), Color.White);
+                    break;
+
+                case HIGHSCORE:
+                    spriteBatch.Draw(highscoreText, new Rectangle(300, 300, highscoreText.Width, highscoreText.Height), Color.White);
+                    break;
+
+                case PLAYGAME:
+                    //What we want to happen when we play our GAME goes in here.
+                    spriteBatch.Draw(playgameText, new Rectangle(300, 100, playgameText.Width, playgameText.Height), Color.White);
+                    spriteBatch.Draw(playgameText, new Rectangle(300, 200, playgameText.Width, playgameText.Height), Color.White);
+                    spriteBatch.Draw(playgameText, new Rectangle(300, 300, playgameText.Width, playgameText.Height), Color.White);
+                    break;
+
+                case GAMEOVER:
+                    //What we want to happen when our GAME is OVER goes in here.
+                    break;
+
+            }
             tileEngine.Draw(gameTime, spriteBatch);
             spriteBatch.End();
             base.Draw(gameTime);
