d972e9604eedbe6e3f621be073eb928a370ba470
CrimsKrams <krille21@live.com>
2018-09-10 08:57:52 +0200

    ja

--- BeeSouls/BeeSouls/MenuState.cs
+++ BeeSouls/BeeSouls/MenuState.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BeeSouls
+﻿namespace BeeSouls
 {
-    class MenuState
+    internal class MenuState
     {
     }
 }
